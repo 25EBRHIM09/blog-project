7fdbe188ff9e25901b6045d1cf7abf6b65ddf4dc
25EBRHIM09 <147305030+25EBRHIM09@users.noreply.github.com>
2024-03-07 07:59:16 +0000

    vid 3

---
+++ src/components/Mainlayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mainlayout = () => {
+  return (
+    <div>
+     <Header /> 
+    </div>
+  )
+}
+
+export default Mainlayout
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const homepage = () => {
+  return <div>HomePage</div>
+  
+};
+
+export default HomePage
+
